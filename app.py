4f0df79265b99d639b415e254ef6e29a99b75921
Alokzhan <alokverma7985354732@gmail.com>
2025-07-07 17:52:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,4 +162,6 @@ def log_history(action, filename):
             con.commit()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
